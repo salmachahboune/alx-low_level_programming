db5def42c936b737aca7a79ca728ea08de50057a
salmachahboune <salmachahboune2003@gmail.com>
2023-03-16 11:29:35 -0700

    www

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
-
+/**
+	* main - A program that prints a line with puts function
+	* Return : 0 (Succes)
+	*/
 	int main(void) {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
