dbe80d91dbf374add31adc9ba898cdcedc7f199e
salmachahboune <salmachahboune2003@gmail.com>
2023-03-27 12:50:26 -0700

    wertyui

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,1 +1,15 @@
-#include
+#include"main.h"
+#include<stdio.h>
+/**
+*_puts- check the code
+*@str:string
+*
+*/
+void _puts(char *str)
+{
+while (*str != '\0')
+{
+_putchar(*str++);
+}
+_putchar('\n');
+}
