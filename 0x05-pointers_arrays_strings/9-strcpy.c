a1a795736a3d13ee4e8d727f0f81d87f3cbd2854
salmachahboune <salmachahboune2003@gmail.com>
2023-03-27 14:01:16 -0700

    wertyui

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,26 +1,21 @@
 #include"main.h"
-
-
-
 /**
 * _strcpy - check the code
 *@dest:string
 *@src:string
-*
+*return : string
 */
-
-
 char *_strcpy(char *dest, char *src)
 {
-int i
+int i;
 int  l = 0;
-int x = 0;
+int x ;
 
 while (*(src + 1) != '\0')
 {
 l++;
 }
-for( ; x < l ; x++)
+for (x = 0 ; x < l ; x++)
 {
 dest[x] = src[x];
 }
