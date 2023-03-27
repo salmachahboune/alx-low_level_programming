82a643986a89775ac6f51e576ee86148fdc298da
salmachahboune <salmachahboune2003@gmail.com>
2023-03-27 12:37:55 -0700

    wertyui

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include"main.h"
+
+/**
+* _strlen - check the code
+*@s:string
+* Return: lenght
+*/
+int _strlen(char *s)
+{
+int longe=0;
+while  (*s != '/0')
+{
+longe++;
+s++;
+}
+return(longe);
+}
